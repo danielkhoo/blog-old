2404e1bdac5d0a940d54b2cf04cc38c284a99722
Daniel Khoo <danieljkhoo@gmail.com>
2019-10-20 16:36:33 +0800

    Add navbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,6 @@ import { Link } from "gatsby"
 import "./layout.scss"
 import './Overview/Overview.scss';
 import Profile from "./Profile/Profile"
-import { Grid, GridColumn } from 'semantic-ui-react';
 enum ActiveTab {
   Blog = 'blog',
   Projects = 'projects'
@@ -25,15 +24,24 @@ const Layout = (props: IProps): JSX.Element => {
   }, []);
   return (
     <>
+      <header>
+        <div className='header-content'>
+          {/* <Link to='/'><div className='logo'>danielkhoo.github.io</div></Link> */}
+          <nav>
+            <Link to='/'><div className={`navitem ${activeTab === ActiveTab.Blog ? 'active' : ''}`}>Blog</div></Link>
+            <Link to='/projects'><div className={`navitem ${activeTab === ActiveTab.Projects ? 'active' : ''}`}>Projects</div></Link>
+          </nav>
+        </div>
+      </header>
       <div className="container">
         {props.showSideBar && <div className="sidebar-wrapper">
           <Profile />
         </div>}
         <div className="main-wrapper">
-          <div className='tab-bar'>
+          {/* <div className='tab-bar'>
             <Link to='/'><button className={activeTab === ActiveTab.Blog ? 'active' : ''}> Blog</button></Link>
             <Link to='/projects'><button className={activeTab === ActiveTab.Projects ? 'active' : ''}> Projects</button></Link>
-          </div>
+          </div> */}
           {props.children}
         </div>
       </div>
