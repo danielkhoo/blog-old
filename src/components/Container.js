8d175fac24a29e0f6b2bcee868c1477a0ac563bc
danielkhoo <danieljkhoo@gmail.com>
2021-06-19 19:16:01 +0800

    fix: add bottom padding to container

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,29 +1,9 @@
-import { Flex, useColorMode } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react';
 
 export const Container = (props) => {
-  const { colorMode } = useColorMode()
-
-  const bgColor = { light: 'gray.50', dark: 'gray.900' }
-
-  const color = { light: 'black', dark: 'white' }
   return (
-    <Flex
-      minHeight="100vh"
-      direction="column"
-      justifyContent="start"
-      alignItems="center"
-    >
-      <Flex
-        maxWidth="800px"
-        width="100%"
-        minHeight="100vh"
-        direction="column"
-        // bg="#CCC"
-        // bg={bgColor[colorMode]}
-        color={color[colorMode]}
-        {...props}
-      />
+    <Flex minHeight="100vh" direction="column" justifyContent="start" alignItems="center">
+      <Flex maxWidth="800px" width="100%" minHeight="100vh" paddingBottom="80px" direction="column" {...props} />
     </Flex>
-
-  )
-}
+  );
+};
