5157c7439eedf04179ebd941663842c246bb82c9
danielkhoo <danieljkhoo@gmail.com>
2021-06-20 23:00:44 +0800

    feat: add seo to app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme/theme';
-
+import { DefaultSeo } from 'next-seo';
+import SEO from '../../next-seo.config';
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </ChakraProvider>
   );
